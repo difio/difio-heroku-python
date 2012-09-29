a656ce48f501e4321bb1f51e9803a29cf4662859
Alexander Todorov <altodorov@dif.io>
2012-09-29 14:14:54 +0300

    update to match Heroku

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 def get_name_version():
     basedir = os.path.dirname(__file__)
-    with open(os.path.join(basedir, 'difio_openshift_python/version.py')) as f:
+    with open(os.path.join(basedir, 'difio_heroku_python/version.py')) as f:
         (name, version) = (None, None)
         exec(f.read())
         return (name, version)
@@ -19,13 +19,13 @@ with open('README.rst') as file:
 setup(
     name=name,
     version=version,
-    description='Difio registration agent for OpenShift / Python applications',
+    description='Difio registration agent for Heroku / Python applications',
     author='Alexander Todorov',
-    author_email='atodorov@nospam.otb.bg',
-    url = 'http://github.com/difio/difio-openshift-python',
-    packages=['difio_openshift_python'],
-    scripts=['difio-openshift-python'],
-    keywords = ['openshift', 'difio', 'updates', 'cloud'],
+    author_email='atodorov@nospam.dif.io',
+    url = 'http://github.com/difio/difio-heroku-python',
+    packages=['difio_heroku_python'],
+    scripts=['difio-heroku-python'],
+    keywords = ['heroku', 'difio', 'updates', 'cloud', 'diff'],
     classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
